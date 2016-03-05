636b2cf8d806286c082ce83d98d3557e0bc0e907
FIRST_NAME LAST_NAME <YOUR_EMAIL>
2016-03-05 13:12:45 -0500

    staying with it...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,61 @@
-function averagePrice(items) {
+var average;
+
+// function averagePrice(items) {
 	var priceSum =
-	items.reduce(function(totalCost, currentItem) {
-		return totalCost + currentItem;
+	items.reduce(function(totalCost, obj) {
+		return totalCost += obj.price;
 	}, 0);
-		var average = priceSum / items.length;
-		return average
-};
-var answerOne = document.querySelector(.averageCost);
-answerOne.textContent= averagePrice(items);
+
+	 average = priceSum / items.length;
+		// return average
+	// };
+
+	// console.log(average);
+var answerOne = document.querySelector(".averageCost");
+answerOne.textContent= average.toFixed(2);
+
+// console.log(average);
+
+
+// function myMyArray(prices) {
+// 	var prices = items.price;
+// 	prices.filter(function(currentPrice){
+// 		if (prices > 14 && prices < 18) {
+// 			return 
+// 		}
+// });
+
+var rightPrice = items.filter(function(item){
+	var newArray = items.price > 14 && items.price < 18;
+		 });
+
+var newArray = 	rightPrice.map(function(price) {
+	  	return price.title;
+	  
+	 
+});
+var answerTwo = document.querySelector(".fitItems");
+answerTwo.textContent = newArray////
+     //console.log(newArray);
+
+// var answerTwo = document.querySelector('.fitItems');
+// answerTwo.textContent = myArray(items);
+
+
+
+	 var singleCode = items.filter(function(currentCode) {
+	  	return currentCode.currency_code === "GPB";
+		 
+	  });
+	 var theOne = singleCode.map(function(item) {
+	 	return item.currency_code;
+	 });
+		  // console.log(oneCode);
+	var answerThree = document.querySelector("gCode");
+	answerThree.textContent = theOne;
+
+// function myWood(items) {
+	// materials.indexOf(wood,[0]) {
+		// return materials.wood;
+	// };
+// };
